05ff8e6b9612473de60097269ca0b565ce5156bc
anaangelieri <131811398+anaangelieri@users.noreply.github.com>
2025-04-16 15:02:43 -0300

    atualizando bottom_nav_bar

--- exercicio2/lib/widgets/bottom_nav_bar.dart
+++ exercicio2/lib/widgets/bottom_nav_bar.dart
@@ -3,34 +3,40 @@ import 'package:flutter/material.dart';
 class BottomNavBar extends StatelessWidget {
   final Function(int) onTabSelected;
 
-  const BottomNavBar({Key? key, required this.onTabSelected}) : super(key: key); //construtor
+  const BottomNavBar({Key? key, required this.onTabSelected}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    //lista de icones
+    final icons = [
+      Icons.menu,
+      Icons.collections,
+      Icons.apps,
+      Icons.info,
+    ];
+
     return BottomAppBar(
-      shape: CircularNotchedRectangle(),
-      notchMargin: 6.0,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () => onTabSelected(0),
-          ),
-          IconButton(
-            icon: Icon(Icons.collections),
-            onPressed: () => onTabSelected(1),
-          ),
-          SizedBox(width: 48), 
-          IconButton(
-            icon: Icon(Icons.apps),
-            onPressed: () => onTabSelected(2),
-          ),
-          IconButton(
-            icon: Icon(Icons.info),
-            onPressed: () => onTabSelected(3),
-          ),
-        ],
+        children: List.generate(icons.length, (index) {
+          // espaço para o FAB no meio 
+          if (index == 2) {
+            return Row(
+              children: [
+                const SizedBox(width: 48),
+                IconButton(
+                  icon: Icon(icons[index]),
+                  onPressed: () => onTabSelected(index),
+                ),
+              ],
+            );
+          }
+
+          return IconButton(
+            icon: Icon(icons[index]),
+            onPressed: () => onTabSelected(index),
+          );
+        }),
       ),
     );
   }
